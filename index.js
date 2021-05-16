c00bada0129e306ee2b2573dfed46eb135585b5e
npty <78221556+npty@users.noreply.github.com>
2021-05-16 16:55:28 +0700

    Full code for covalent article (#1)
    * Fix bugs for bsc
    
    * Colorize output
    
    * Cleanup code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Axios = require('axios').default
+const chalk = require('chalk');
 
 /**
  * Fetch transactions which has `Swap` event from given chainId and wallet address.
@@ -27,99 +28,167 @@ const Axios = require('axios').default
  */
 
 async function getSwapTransactions(chainId, address) {
-  // Step 1: Fetches token details for given wallet address
-  const _balances = await Axios.get(`https://api.covalenthq.com/v1/${chainId}/address/${address}/balances_v2/`, {
-    params: {
-      'no-nft-fetch': true
-    }
-  }).then(result => result.data.data.items)
+  // Define native token symbol for given chain
+  let nativeTokenSymbol;
+  switch (chainId) {
+    case '1':
+      nativeTokenSymbol = 'ETH'
+      break;
+    case '56':
+      nativeTokenSymbol = 'BNB'
+      break;
+    case '137':
+      nativeTokenSymbol = 'MATIC'
+      break;
+    case '43114':
+      nativeTokenSymbol = 'AVAX'
+      break;
+    case '250':
+      nativeTokenSymbol = 'FTM'
+      break;
+    default:
+      nativeTokenSymbol = 'Native Token'
+  }
+  // Covalent API returns a lower-case address. We have to make sure user-input address is also lowered case.
+  const _address = address.toLowerCase()
 
-  // Step 2: Cache token balance
+  // Step 1: Fetches token details for given wallet address and cache token details
+  const _balances = await Axios.get(`https://api.covalenthq.com/v1/${chainId}/address/${address}/balances_v2/`).then(result => result.data.data.items)
+
+  // Cache token balance
   const _cacheBalanceTokens = []
-  for(const balance of _balances) {
+  for (const balance of _balances) {
     _cacheBalanceTokens[balance.contract_address] = balance
   }
 
-  // Step 3: Fetches transaction history from given chainId and address
+  // Step 2: Fetches transaction history from given chainId and address
   const _transactions = await Axios.get(`https://api.covalenthq.com/v1/${chainId}/address/${address}/transactions_v2/`, {
     params: {
-      'page-size': 300 // Number of transactions per page
+      'page-size': 100 // Number of transactions per page
     }
   })
-  .then(result => result.data.data.items)
+    .then(result => result.data.data.items)
 
-  // Step 4: Filters out transactions that 'Swap' event didn't exist.
-  const _swapTransactions = _transactions.filter(({ log_events }) => log_events.find(({ decoded }) => decoded && decoded.name === 'Swap'))
+  // Step 3: Filtering transactions in the following cases:
+  // 1. Swap event doesn't exist
+  // 2. If transaction value = 0, then make sure there's transfer event from sender
+  // 3. If transaction value != 0, then make sure there's transfer event to sender
+  const _swapTransactions = _transactions.filter(({ log_events, value }) => {
+    const events = log_events
+      .filter(({ decoded }) => decoded)
+      .map(({ decoded }) => decoded)
 
-  // Step 5: Sanitize and format return data
-  const swapTransactions = _swapTransactions.map(transaction => {
-    const _address = address.toLowerCase()
-    // ERC20 Transfer
-    if(transaction.value === "0") {
-      const sentEvent = transaction.log_events.find(({decoded}) => {
-        return decoded && decoded.name === 'Transfer' && decoded.params[0].value === _address
-      })
-      const receiveErc20Event = transaction.log_events.find(({decoded}) => {
-        return decoded && decoded.name === 'Transfer' && decoded.params[1].value === _address
-      })
+    const eventNames = events
+      .map(({ name }) => name)
 
-      // Receive ERC20 token
-      if(receiveErc20Event) {
-        return {
-          txHash: transaction.tx_hash,
-          gasQuote: transaction.gas_quote,
-          timestamp: transaction.block_signed_at,
-          fromToken: sentEvent.sender_address,
-          fromTokenSymbol: _cacheBalanceTokens[sentEvent.sender_address].contract_ticker_symbol,
-          fromTokenAmount: sentEvent.decoded.params[2].value,
-          fromTokenDecimal: _cacheBalanceTokens[sentEvent.sender_address].contract_decimals,
-          toToken: receiveErc20Event.sender_address,
-          toTokenSymbol: _cacheBalanceTokens[receiveErc20Event.sender_address].contract_ticker_symbol,
-          toTokenAmount: receiveErc20Event.decoded.params[2].value,
-          toTokenDecimal: _cacheBalanceTokens[receiveErc20Event.sender_address].contract_decimals
-        }
+    // If there's mint function, it means that this transaction is providing the liquidity, not swap transaction which we want.
+    if (eventNames.indexOf("Mint") !== -1) return false
+    if (eventNames.indexOf("Swap") !== -1) {
+      const transferEvents = events.filter(({ name }) => name === "Transfer")
+      if (value === '0') {
+        return transferEvents.find(({ params }) => params[0].value === _address)
       } else {
-        // Receive ETH
-        const receiveEthEvent = transaction.log_events.find(({decoded}) => {
-          return decoded && decoded.name === 'Withdrawal'
-        })
-
-        return {
-          txHash: transaction.tx_hash,
-          timestamp: transaction.block_signed_at,
-          gasQuote: transaction.gas_quote,
-          fromToken: sentEvent.sender_address,
-          fromTokenSymbol: _cacheBalanceTokens[sentEvent.sender_address].contract_ticker_symbol,
-          fromTokenAmount: sentEvent.decoded.params[2].value,
-          fromTokenDecimal: _cacheBalanceTokens[sentEvent.sender_address].contract_decimals,
-          toToken: 'ETH',
-          toTokenSymbol: 'Ether',
-          toTokenAmount: receiveEthEvent.decoded.params[1].value,
-          toTokenDecimal: 18
-        }
+        return transferEvents.find(({ params }) => params[1].value === _address)
       }
-    } else { // ETH Transfer
-      const receiveEvent = transaction.log_events.find(({decoded}) => {
-        return decoded && decoded.name === 'Transfer' && decoded.params[1].value === _address
-      })
+    }
+  })
 
+  // Step 4: Sanitize and format return data
+  const swapTransactions = _swapTransactions.map(transaction => {
+    const {
+      fromTokenAmount,
+      fromTokenDecimal,
+      fromTokenSymbol,
+      toTokenAmount,
+      toTokenDecimal,
+      toTokenSymbol,
+      txHash
+    } = getTransferEventParams({ tokens: _cacheBalanceTokens, transaction, nativeTokenSymbol, address: _address })
+
+    return `${chalk.yellowBright('Swapped')} ${chalk.blueBright(fromTokenAmount / (10 ** fromTokenDecimal))} ${chalk.greenBright(fromTokenSymbol)} -> ${chalk.blueBright(toTokenAmount / (10 ** toTokenDecimal))} ${chalk.greenBright(toTokenSymbol)} (${txHash})`
+  })
+
+  for (const swapTx of swapTransactions) {
+    console.log(swapTx)
+  }
+}
+
+
+function getTransferEventParams({ tokens, transaction, nativeTokenSymbol, address }) {
+  const transferEvents = transaction.log_events
+    .filter(({ decoded }) => decoded && decoded.name === 'Transfer')
+
+  const transferSentEvents = transferEvents
+    .filter(({ decoded }) => decoded.params[0].value === address)
+    .map(({ sender_address, decoded }) => ({
+      token: sender_address,
+      value: decoded.params[2].value
+    }))
+
+  const _transferReceiveErc20Event = transferEvents
+    .find(({ decoded }) => decoded.params[1].value === address)
+
+  let transferReceiveErc20Event;
+  if (_transferReceiveErc20Event) {
+    transferReceiveErc20Event = {
+      token: _transferReceiveErc20Event.sender_address,
+      value: _transferReceiveErc20Event.decoded.params[2].value
+    }
+  }
+
+  const _transferReceiveEthEvent = transaction.log_events.find(({ decoded }) => {
+    return decoded && decoded.name === 'Withdrawal'
+  })
+
+  let transferReceiveEthEvent;
+  if (_transferReceiveEthEvent) {
+    transferReceiveEthEvent = {
+      token: 'ETH',
+      value: _transferReceiveEthEvent.decoded.params[1].value
+    }
+  }
+
+  // Use ERC20 to swap
+  if (transaction.value === "0") {
+    // For some tokens, it has fee or burn mechanism for every transfers.
+    // In this case, we will support it by aggregates the value of every transfer that being sent from the given address.
+    const sentEvent = transferSentEvents.reduce((acc, sentEvent) => ({ ...acc, value: parseInt(acc.value) + parseInt(sentEvent.value) }))
+
+    // Swap ERC20 to ERC20
+    if (transferReceiveErc20Event) {
       return {
         txHash: transaction.tx_hash,
-        gasQuote: transaction.gas_quote,
-        timestamp: transaction.block_signed_at,
-        fromToken: 'ETH',
-        fromTokenSymbol: 'Ether',
-        fromTokenAmount: transaction.value,
-        fronTokenDecimals: 18,
-        toToken: receiveEvent.sender_address,
-        toTokenAmount: receiveEvent.decoded.params[2].value,
-        toTokenSymbol: _cacheBalanceTokens[receiveEvent.sender_address].contract_ticker_symbol,
-        toTokenDecimal: _cacheBalanceTokens[receiveEvent.sender_address].contract_decimals
+        fromTokenSymbol: tokens[sentEvent.token].contract_ticker_symbol,
+        fromTokenAmount: sentEvent.value,
+        fromTokenDecimal: tokens[sentEvent.token].contract_decimals,
+        toTokenSymbol: tokens[transferReceiveErc20Event.token].contract_ticker_symbol,
+        toTokenAmount: transferReceiveErc20Event.value,
+        toTokenDecimal: tokens[transferReceiveErc20Event.token].contract_decimals,
+      }
+    } else {
+      // Swap ERC20 to ETH
+      return {
+        txHash: transaction.tx_hash,
+        fromTokenSymbol: tokens[sentEvent.token].contract_ticker_symbol,
+        fromTokenAmount: sentEvent.value,
+        fromTokenDecimal: tokens[sentEvent.token].contract_decimals,
+        toTokenSymbol: nativeTokenSymbol,
+        toTokenAmount: transferReceiveEthEvent.value,
+        toTokenDecimal: 18,
       }
     }
-  })
-
-  console.log(JSON.stringify(swapTransactions))
+  } else {
+    // Swap ETH to ERC20
+    return {
+      txHash: transaction.tx_hash,
+      fromTokenSymbol: nativeTokenSymbol,
+      fromTokenAmount: transaction.value,
+      fromTokenDecimal: 18,
+      toTokenSymbol: tokens[transferReceiveErc20Event.token].contract_ticker_symbol,
+      toTokenAmount: transferReceiveErc20Event.value,
+      toTokenDecimal: tokens[transferReceiveErc20Event.token].contract_decimals
+    }
+  }
 }
 
 getSwapTransactions('1', '0x632A84DC35A1e43B8196B2d08630dC9e6a1F3692')
